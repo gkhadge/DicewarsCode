50d8fcbcb23d96d6611a84f34184c6b06d30d8f9
Gourav Khadge <gkhadge@g.hmc.edu>
2014-06-04 20:32:19 -0700

    Added constants and fixes some errors

--- offsetcoordinatemap.py
+++ offsetcoordinatemap.py
@@ -19,13 +19,23 @@
 #   o           o             v   
 #      o     o          +x direction +y direction 
 #         o 
-# Class uses axial coordinates with pointy-topped hexagons
+# Class uses odd row offset coordinates with pointy-topped hexagons
 # See link for more details
 # http://www.redblobgames.com/grids/hexagons/
 
 # See for code organization and discussion:
 # https://docs.google.com/a/g.hmc.edu/document/d/1qaDAmrd0Lflz5zCO4VxH2M4TKGrQX58B_4DWbv6YZWk/edit
 
+## Directions
+SR = 0 # Straight Right
+UR = 1 # Upper Right
+UL = 2 # Upper Left
+SL = 3 # Straight Left
+LL = 4 # Lower Left
+LR = 5 # Lower Right
+
+## Error code
+INVALID = -1
 
 class Map2:
     # See above.
@@ -34,11 +44,11 @@ class Map2:
         # hexList is a list containing hexagons.
 
         # There's got to be a better way to do this.
-        self.rows = 28
-        self.cols = 32
+        self.ROWS = 28
+        self.COLS = 32
 
         # Store hexagons in 2D array, "grid"       
-        self.grid = [[0 for x in xrange(self.cols)] for x in xrange(self.rows)]
+        self.grid = [[0 for x in xrange(self.COLS)] for x in xrange(self.ROWS)]
         
     def neighbor(self, x, y, direction):
         # Returns state of hexagon in the direction of direction
@@ -50,7 +60,7 @@ class Map2:
         
         if direction < 0 or direction > 5:
             print "Invalid Direction"
-            return -1
+            return INVALID
 
 
         neighbors = [[ [0,  1], [ -1, 0], [-1, -1],[0,  -1], [1, -1], [1, 0] ], \
@@ -61,6 +71,6 @@ class Map2:
         newx = x + d[0]
         newy = y + d[1]
         print str(newx) + ", " + str(newy) 
-        if newx < 0 or newx >= self.rows or newy < 0 or newy >= self.cols:
-            return -1
+        if newx < 0 or newx >= self.ROWS or newy < 0 or newy >= self.COLS:
+            return INVALID
         return self.grid[newx][newy]
